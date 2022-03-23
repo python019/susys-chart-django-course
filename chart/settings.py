42d688b8863174c708b76ac41b60fb0ca31bb8fb
python019 <tommyjumayev@gmail.com>
2022-03-24 03:07:48 +0500

    5-dars

--- chart/settings.py
+++ chart/settings.py
@@ -44,7 +44,7 @@ ROOT_URLCONF = 'chart.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ BASE_DIR / "frontend" ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
