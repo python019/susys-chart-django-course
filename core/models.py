2840a82cf01062ac3fc7fec4906bd8b3e2258608
python019 <tommyjumayev@gmail.com>
2022-03-26 16:07:52 +0500

    9-dars

--- core/models.py
+++ core/models.py
@@ -3,3 +3,7 @@ from django.db import models
 # Create your models here.
 class Post(models.Model):
     nomi = models.CharField(max_length=50)
+    mazmuni = models.TextField()
+
+    def __str__(self):
+        return self.nomi
