d9130211ec74e8d71c4f307294b31a7eecedf4ef
python019 <tommyjumayev@gmail.com>
2022-03-26 16:25:24 +0500

    10-dars

--- core/models.py
+++ core/models.py
@@ -2,8 +2,13 @@ from django.db import models
 
 # Create your models here.
 class Post(models.Model):
+    class Meta:
+        verbose_name = "Post"
+        verbose_name_plural = "Postlar"
+
     nomi = models.CharField(max_length=50)
     mazmuni = models.TextField()
 
     def __str__(self):
         return self.nomi
+
